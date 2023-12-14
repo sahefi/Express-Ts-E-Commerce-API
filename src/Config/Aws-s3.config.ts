df6e7983e785db07547e60ff2d2ac31897050a4b
sahefi <alvianto_putra_29rpl@student.smktelkom-mlg.sch.id>
2023-12-14 08:59:54 +0700

    firs commit

--- src/Config/Aws-s3.config.ts
+++ src/Config/Aws-s3.config.ts
@@ -1,11 +1,11 @@
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-AWS.config.update({
-  accessKeyId: 'YOUR_ACCESS_KEY_ID',
-  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-  region: 'YOUR_REGION', // Replace with your S3 bucket's region
-});
+// AWS.config.update({
+//   accessKeyId: 'YOUR_ACCESS_KEY_ID',
+//   secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+//   region: 'YOUR_REGION', // Replace with your S3 bucket's region
+// });
 
-const s3 = new AWS.S3();
+// const s3 = new AWS.S3();
 
-export default s3
+// export default s3
--- src/services/Common/awsUploadservice.ts
+++ src/services/Common/awsUploadservice.ts
@@ -1,47 +1,47 @@
-import { Request, Response, NextFunction } from 'express';
-import AWS from 'aws-sdk';
-import multer from 'multer';
-import fs from 'fs';
+// import { Request, Response, NextFunction } from 'express';
+// import AWS from 'aws-sdk';
+// import multer from 'multer';
+// import fs from 'fs';
 
-// Configure AWS SDK
-AWS.config.update({
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID!,
-  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
-  region: process.env.AWS_S3_REGION!,
-});
+// // Configure AWS SDK
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID!,
+//   secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
+//   region: process.env.AWS_S3_REGION!,
+// });
 
-// Create an S3 instance
-const s3 = new AWS.S3();
-const upload = multer({ dest: 'uploads/' });
+// // Create an S3 instance
+// const s3 = new AWS.S3();
+// const upload = multer({ dest: 'uploads/' });
 
-const s3UploadMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  upload.single('image')(req, res, async (err: any) => {
-    try {
-      if (err instanceof multer.MulterError) {
-        return res.status(400).send('Multer error: ' + err.message);
-      } else if (err) {
-        return res.status(500).send('Error uploading file: ' + err.message);
-      }
+// const s3UploadMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//   upload.single('image')(req, res, async (err: any) => {
+//     try {
+//       if (err instanceof multer.MulterError) {
+//         return res.status(400).send('Multer error: ' + err.message);
+//       } else if (err) {
+//         return res.status(500).send('Error uploading file: ' + err.message);
+//       }
 
-      if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-      }
+//       if (!req.file) {
+//         return res.status(400).send('No file uploaded.');
+//       }
 
 
-      const params = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME!,
-        Key: req.file.originalname,
-        Body: req.file.path,
-      };
+//       const params = {
+//         Bucket: process.env.AWS_S3_BUCKET_NAME!,
+//         Key: req.file.originalname,
+//         Body: req.file.path,
+//       };
 
-      const uploaded = await s3.upload(params).promise();
-      req.body.image_link = uploaded.Location; // Save the uploaded file URL to the request object
-      next(); // Move to the next middleware or route handler
-    } catch (error) {
-      console.error('Error uploading file:', error);
-      return res.status(500).send('Error uploading file: ' + error.message);
-    }
-  });
-};
+//       const uploaded = await s3.upload(params).promise();
+//       req.body.image_link = uploaded.Location; // Save the uploaded file URL to the request object
+//       next(); // Move to the next middleware or route handler
+//     } catch (error) {
+//       console.error('Error uploading file:', error);
+//       return res.status(500).send('Error uploading file: ' + error.message);
+//     }
+//   });
+// };
 
-export default s3UploadMiddleware;
+// export default s3UploadMiddleware;
